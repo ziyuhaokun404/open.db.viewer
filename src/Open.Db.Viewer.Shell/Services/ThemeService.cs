1d7e4de64165349b0d88de1db9c4ff9313b7f369
ziyuhaokun <ziyuhaokun@qq.com>
2026-05-05 10:26:38 +0800

    Remove custom AppTheme resources

--- src/Open.Db.Viewer.Shell/Services/ThemeService.cs
+++ src/Open.Db.Viewer.Shell/Services/ThemeService.cs
@@ -67,7 +67,6 @@ public sealed class ThemeService : IDisposable
     {
         EffectiveTheme = applicationTheme;
         ApplicationThemeManager.Apply(applicationTheme);
-        ThemeResourceManager.Apply(applicationTheme);
         ThemeChanged?.Invoke(this, EventArgs.Empty);
     }
 
